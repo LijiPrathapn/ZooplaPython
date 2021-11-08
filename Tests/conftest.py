744982db2a14fb098639ffbf1ea04f44abbbf0ab
LijiPrathapn <liji.prathapan@gmail.com>
2021-11-07 20:39:06 -0800

    Update

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture()
 def setup():
-    driver=webdriver.Chrome(executable_path="C:\\Users\\lijin\\Downloads\\chromedriver_win32 (5)\\chromedriver.exe")
+    driver=webdriver.Chrome(executable_path="C:\\Users\\lijin\\Downloads\\chromedriver_win32 (6)\\chromedriver.exe")
     driver.maximize_window()
 
     return driver
